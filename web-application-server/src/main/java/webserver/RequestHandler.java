ab4c22003ca0eac2b9f9a2b34b3b269557fd0444
wjddnjsrbs97@naver.com <wjddnjsrbs97@naver.com>
2023-05-06 14:47:21 +0900

    2023 05 06

--- web-application-server/src/main/java/webserver/RequestHandler.java
+++ web-application-server/src/main/java/webserver/RequestHandler.java
@@ -8,10 +8,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.http.HttpRequest;
 import java.nio.file.Files;
 import java.util.Map;
 
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,33 +35,41 @@ public class RequestHandler extends Thread {
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = br.readLine();
+			String[] tokens = line.split(" ");
+			
+			int contentLength = 0;
+			
 
-			// log.debug("request line : {}",line);
-
+			while(!line.equals("")) {
+				line = br.readLine();
+				if(line.contains("Content-Length")){
+					contentLength = getContentLength(line);
+				}
+			}
+			
 			// 만약에 들어온 url이 없으면 리턴해준다 -> 무한루프 막기 위해서
 			if (line == null)
 				return;
-			
-			String[] tokens = line.split(" ");
-			
+
 			String url = tokens[1];
 			
-			///1. user/create로 시작하는 URL이 들어올 때 처리해줌 
-			if (url.startsWith("/user/create")) {
-				//2. create?~~ 에서 ?의 위치를 저장
-				int index = url.indexOf("?");
+			if ("/user/create".equals(url)) {
+				String body = IOUtils.readData(br, contentLength);
+
+				//userId=admin&password=1234...로 들어왔을 때 = 을 기준으로 key:value parsing
+				Map<String, String> datas = HttpRequestUtils.parseQueryString(body);
 				
-				//3. ?이후의 url을 핵심 url로 자른다
-				String pointUrl = url.substring(index+1);
-				
-				//4. ?이후에 userId=admin&password=1234...로 들어왔을 때 = 을 기준으로 key:value parsing
-				Map<String, String> datas = HttpRequestUtils.parseQueryString(pointUrl);
-				
-				//5. User 객체 생성 
+				//User 객체 생성 
 				User user = new User(datas.get("userId"),datas.get("password"),datas.get("name"),datas.get("email"));
 				log.debug("user info: {}",user);
+				
+				//response 헤더를 만들기 위해 output 객체를 만들고 이동할 url로 바꿔준다
+				DataOutputStream dos = new DataOutputStream(out);
+				url = "/index.html";
+				response302Header(dos, url);
 			}
 			
 			else {
@@ -72,6 +82,11 @@ public class RequestHandler extends Thread {
 			log.error(e.getMessage());
 		}
 	}
+	
+	private int getContentLength(String line) {
+		String[] headerTokens = line.split(":");
+		return Integer.parseInt(headerTokens[1].trim());
+	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
 		try {
@@ -83,6 +98,17 @@ public class RequestHandler extends Thread {
 			log.error(e.getMessage());
 		}
 	}
+	
+	private void response302Header(DataOutputStream dos, String url) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Location: " + url + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+	
 
 	private void responseBody(DataOutputStream dos, byte[] body) {
 		try {
